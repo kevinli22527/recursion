c3e4102ef2315cb5ae6af2c81cdf2ce80c9bb568
kevin <kevinli22527@yahoo.com>
2020-08-24 21:57:48 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/Project6_starter_files.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project6_starter_files.dir/Project6.cpp.o"
+  "CMakeFiles/Project6_starter_files.dir/main.cpp.o"
+  "Project6_starter_files.exe"
+  "Project6_starter_files.pdb"
+  "libProject6_starter_files.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project6_starter_files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
